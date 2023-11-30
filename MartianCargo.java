af2df1f4cb3b58386790a5812db38d7b610b151f
Aikan <aikan.abdrashitova@alatoo.edu.kg>
2023-11-30 13:13:14 +0600

    second commit

--- MartianCargo.java
+++ MartianCargo.java
@@ -6,7 +6,7 @@ public class MartianCargo {
     private static Scanner scanner = new Scanner(System.in);
     private static Random random = new Random();
     private static ArrayList<Integer> boxLocations = new ArrayList<>();
-    private static int attempts = 0;
+    private static int attempts = 0; //for commit
 
     public static void main(String[] args) {
         initializeBoxLocations();
